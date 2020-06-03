79f582cba9ea0f3f69eb3187a9c9672dcf1b9798
Ash <42413801+ashm97@users.noreply.github.com>
2020-06-03 13:30:05 +0100

    Save Mappings

--- anonymise_patients.r
+++ anonymise_patients.r
@@ -5,7 +5,7 @@
 ##
 ## Author: Mr. Ashleigh C. Myall
 ##
-## Date Created: 26-05-2020
+## Date Created: 26-06-2020
 ##
 ## Copyright (c) Ashleigh C. Myall 2020
 ## Email: a.myall19@imperial.ac.uk
@@ -23,7 +23,8 @@
 ################################################################################
 
 library(digest)
-library(readr)
+library(RODBC)
+#library(readr)
 
 ################################################################################
 
@@ -98,10 +99,19 @@ salt <- function(.x, .seed = NULL, .chars = letters, .n_chars = 5L) {
 }
 
 
+# ------------------------------------------------------------------------------
+
+#' Anon Wrapper
+#' 
+
+wrap_anon = function(x,prefix = ""){
+  return(paste(prefix,salt(anonymize(x), .n_chars = 5L),sep = "_"))
+}
+
 ################################################################################
 
 ##########
-##########                       Example Work Flow 
+##########                       Work Flow 
 ##########
 
 ################################################################################
@@ -113,30 +123,42 @@ salt <- function(.x, .seed = NULL, .chars = letters, .n_chars = 5L) {
 
 
 
-# Example
-string_example = "First-name Last-name"
-
-# Example String
-print(string_example)
-
-# Run it through function once
-print(anonymize(string_example))
-
-# Same string running through function maps to same output
-print(anonymize(string_example))
-
-
-
-
 # Load in data
-patient_data <- read_csv("patient_data.csv")
+#patient_data <- read.csv("patient_data.csv")
+con <- odbcDriverConnect(connection="driver={SQL Server};server=SQL028-INFCT-L\\INST1;database=IC_STG;trusted_connection=yes;")
+patient_data <- sqlQuery(con, "SELECT * FROM [IC_STG].[dbo].[CPE_pathways] ")
+
+
+
+
+
+# Drop columns 
+drop = c("PtName","NHSNo" ,"PtDoB","Tests Performed","Site" ,"AdmDT","DisDT","AdmitDateTime","CurrentLastFacility","EncounterType","Sex","DateOfBirth","DateOfDeath","CollectMinus7DT")
+p2_data = patient_data[,!(names(patient_data) %in% drop)]
+
 
 # Choose columns to anoynomize
-cols_to_mask <- c("full_name","ethnicity") # <-------- Replace columns to match your data
-col_index = which(colnames(patient_data) %in% cols_to_mask)
+#cols_to_mask <- c("PtNumber","Accnumber") # <-------- Replace columns to match your data
+#col_index = which(colnames(p2_data) %in% cols_to_mask)
 
 # backup original data
-patient_data_org <- patient_data
+p2_data_org <- p2_data
+
+# Gererate Mapping
+df_mapping = data.frame("PtNumber" = p2_data$PtNumber, "Accnumber" = p2_data$Accnumber)
+
 
 # Anonymize each specified column
-patient_data[col_index] <- lapply(patient_data[col_index], anonymize)
+#patient_data[col_index] <- lapply(patient_data[col_index], anonymize)
+p2_data$PtNumber = wrap_anon(p2_data$PtNumber,"PtNum")
+p2_data$Accnumber = wrap_anon(p2_data$Accnumber,"Acnum")
+
+df_mapping$PtNumber_anon = p2_data$PtNumber
+df_mapping$Accnumber_anon  = p2_data$Accnumber
+
+
+
+
+# Save Data
+write.csv(df_mapping,"data_mapping.csv")
+saveRDS(p2_data, "anon_patient_data.rds") # <--- save as RDS
